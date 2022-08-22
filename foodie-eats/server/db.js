88067e01b7f077f99215e73dbb9a3eaa14e29d4e
Celene Saw <azadesu.github@gmail.com>
2022-08-22 18:27:10 +1000

    refactoring database

---
+++ foodie-eats/server/db.js
@@ -0,0 +1,23 @@
+// set up db connection
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const DB = "mongodb+srv://allforone:2022sem2itp4llforone@all-for-one.p09tmlv.mongodb.net/?retryWrites=true&w=majority"|| "mongodb://localhost:27017";
+
+// connect to the DB
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  dbName: "trukkDB",
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error: "));
+
+db.once("open", () => {
+  console.log("connected to MongoDB!");
+});
+
+module.exports = db;
