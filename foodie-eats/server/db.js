4fa0ac655c8503474f180eb13d5a0eabe636ad98
Celene Saw <azadesu.github@gmail.com>
2022-08-22 18:29:53 +1000

    database name

--- foodie-eats/server/db.js
+++ foodie-eats/server/db.js
@@ -2,14 +2,14 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-const DB = "mongodb+srv://allforone:2022sem2itp4llforone@all-for-one.p09tmlv.mongodb.net/?retryWrites=true&w=majority"|| "mongodb://localhost:27017";
+const DB = "mongodb+srv://allforone:2022sem2itp4llforone@all-for-one.p09tmlv.mongodb.net/?retryWrites=true&w=majority"|| "mongodb://localhost:27017/foodie-eats";
 
 // connect to the DB
 mongoose.connect(DB, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-  dbName: "trukkDB",
+  dbName: "foodie-eats",
 });
 
 const db = mongoose.connection;
