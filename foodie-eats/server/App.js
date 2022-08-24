b24248e4fa55fa386e246fb389b63bd0527f8786
Celene Saw <azadesu.github@gmail.com>
2022-08-24 20:04:19 +1000

    added svg logos

--- foodie-eats/server/App.js
+++ foodie-eats/server/App.js
@@ -1,6 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+require("dotenv").config();
+require("./db");
+require("./config/passport")(passport);
+const http = require("http");
+const express = require("express");
+const session = require("express-session");
+const cors = require("cors");
+const socketio = require("socket.io");
+const passport = require("passport");
 
+// const userRoutes = require("./routes/passport/userRoutes");
+// const customerRoutes = require("./routes/customer/customerRoutes");
+// const vendorRoutes = require("./routes/vendor/vendorRoutes");
+// const orderRoutes = require("./routes/order/orderRoutes");
+
+
+// initialise express server
+const app = express();
+// enable cors for use of api in client
+app.use(cors());
+const port = process.env.PORT || 4000;
+const server = http.createServer(app);
+const io = socketio(server, {
+  cors: {
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+require("./config/sockets")(io);
+app.set("io", io);
 function App() {
   return (
     <div className="App">
