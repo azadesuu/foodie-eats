438e645ddc86c9ed5a5a858294b47f149b589c62
Claudya <Claudya@Clauds-MacBook-Pro.local>
2022-09-30 14:11:20 +0700

    Added build version

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const SERVER_URL = "http://localhost:5000"; //server url
+//const SERVER_URL = "http://localhost:5000"; //server url
+const SERVER_URL = process.env.SERVER_URL; //server url
 
 export const setAuthToken = token => {
     if (token) {
