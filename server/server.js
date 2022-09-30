05bfa9faa2666b26f474e51974fbfb84fa44f824
Claudya <Claudya@Clauds-MacBook-Pro.local>
2022-09-30 20:08:37 +0700

    Added Control Allow Origin headers

--- server/server.js
+++ server/server.js
@@ -19,6 +19,16 @@ app.use(
   })
 );
 
+// Add Access Control Allow Origin headers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 /*app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin", "X-Requested-With, Content-Type, Accept");
