edca8586b27be76075d67faeb2b9d453d207eec9
Celene Saw <azadesu.github@gmail.com>
2022-10-06 22:46:13 +1100

    fixed redirect error

--- client/src/App.js
+++ client/src/App.js
@@ -64,7 +64,7 @@ function App() {
                 <div>
                     <Routes>
                         {/* public routes */}
-                        <Navigate from="/" exact to="/home" />
+                        <Route path="/" element={<Navigate to="/home" />} />
                         <Route path="/home" element={<Community />} />
                         <Route
                             path="/review/:reviewId"
