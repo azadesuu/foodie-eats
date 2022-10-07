54c02bd8da25529aecf5f5c0083a36dfecbbdb55
Joeann Chong <joeannc@student.unimelb.edu.au>
2022-10-07 15:40:22 +1100

    added Profile and ProfileReview pages

--- client/src/components/Profile/index.js
+++ client/src/components/Profile/index.js
@@ -2,169 +2,244 @@ import "./Profile.css";
 
 import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../../actions/UserContext";
-import { useParams } from "react-router-dom";
-import { navigate, useNavigate } from "react";
-import { getProfile } from "../../api";
-import { useQuery, useMutation } from "react-query";
-import Axios from "axios";
+import { useParams, useNavigate } from "react-router-dom";
+import { getProfile, getOtherReviews } from "../../api";
+import { useQuery } from "react-query";
 import { CircularProgress } from "@mui/material";
-import { usePreviousNonNullish } from "../../hooks";
 
-import EditIcon from "@mui/icons-material/Edit";
+import "@fontsource/martel-sans";
 
 import Avatar from "@mui/material/Avatar";
 import IconButton from "@mui/material/IconButton";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 
-import EditProfile from "../EditProfile";
-import ChangePassword from "../ChangePassword";
+import SearchIcon from "@mui/icons-material/Search";
+import FilterAltIcon from "@mui/icons-material/FilterAlt";
 
-// function TopUser() {
-//     //getting visited user
-//     // const navigate = useNavigate();
-//     // const { username } = useParams();
+import ReviewPeek from "../ReviewPeek";
 
-//     // getting logged in user
-//     // const [user, setUser] = useContext(UserContext);
-//     // const [loggedUsername, setloggedUsername] = useState("");
-//     // useEffect(() => {
-//     //     setloggedUsername(user?.username);
-//     // }, [user]);
+function SearchBar() {
+    const data = [
+        { Name: "Calia", Author: "abcd123" },
+        { Name: "David's Hotpot", Author: "xyz789" }
+    ];
+    return (
+        <div className="searchbar">
+            <div className="searchrow">
+                <SearchIcon />
+                <input
+                    type="text"
+                    placeholder="Search"
+                    name="search"
+                    id="search"
+                    // value="{{search}}"
+                    required
+                />
+                <FilterAltIcon />
+            </div>
+            <div className="searchResult"></div>
+        </div>
+    );
+}
 
-//     // if its the  logged in user, load the myprofile component
-//     // const isUser = username === loggedUsername;
-//     // if (isUser) {
-//     //     navigate("/my-profile");
-//     // }
-//     const { data: userProfile, isLoading } = useQuery(
-//         "profile",
-//         () => getProfile(props.username),
-//         { enabled: !!username }
-//     );
+function TopUser(props) {
+    const userProfile = props.user;
+    return (
+        <div className="top-user">
+            <div className="top-user-r1">
+                <Avatar
+                    alt="user-profile-image"
+                    // src={userProfile.profileImage}
+                    sx={{ height: 130, width: 130 }}
+                />
+                <div className="top-user-info">
+                    <h2>{userProfile.username}</h2>
+                    <p>{userProfile.bio}</p>
+                </div>
+            </div>
+            <div className="top-user-rev">
+                <p>
+                    <span className="detail">7</span> reviews
+                </p>
+                <p>
+                    <span className="detail">10k</span> likes
+                </p>
+            </div>
+        </div>
+    );
+}
 
-//     return (
-//         <div className="top-user">
-//             {/* {isLoading && <CircularProgress className="spinner" />}
-//             {userProfile ? ( */}
-//             <div>
-//                 {/* <div className="top-user-r1">
-//                         <Avatar
-//                             alt="user-profile-image"
-//                             // src={userProfile.profileImage}
-//                             sx={{ height: 130, width: 130 }}
-//                         />
-//                         <div className="top-user-info"> */}
-//                 {/* <h2>{userProfile.username}</h2>
-//                             <p>{userProfile.bio}</p> */}
-//                 {/* </div>
-//                     </div>
-//                     <div className="top-user-rev">
-//                         <p>
-//                             <span className="detail">7</span> reviews
-//                         </p>
-//                         <p>
-//                             <span className="detail">10k</span> likes
-//                         </p>
-//                     </div> */}
-//             </div>
-//             {/* ) : (
-//                 <h1>User not found</h1>
-//             )} */}
-//         </div>
-//     );
-// }
+function Sidebar(props) {
+    const userProfile = props.user;
+    const navigate = useNavigate();
 
-// function Sidebar() {
-//     return (
-//         <div className="sidebar-content">
-//             <div id="current">
-//                 <a href="reviews">reviews</a>
-//             </div>
-//         </div>
-//     );
-// }
+    return (
+        <div className="sidebar-content">
+            <div id="current">
+                <a onClick={() => {
+                    navigate(`/profile/${userProfile.username}/reviews`);
+                    }}
+                >
+                    reviews
+                </a>
+            </div>
+        </div>
+    );
+}
 
-// function ProfileDetails() {
-//     //getting visited user
-//     const navigate = useNavigate();
-//     const { username } = useParams();
+function ReviewsBigScreen(props) {
+    const user = props.user;
+    const { data: listReviews, isLoading } = useQuery(
+        "reviews",
+        () => getOtherReviews(user?._id),
+        { enabled: !!user }
+    );
 
-//     // getting logged in user
-//     const [user, setUser] = useContext(UserContext);
-//     const [loggedUsername, setloggedUsername] = useState("");
-//     useEffect(() => {
-//         setloggedUsername(user?.username);
-//     }, [user]);
+    return (
+        <div className="reviews">
+            <div className="reviews-r1">
+                <h2>reviews</h2>
+                <SearchBar />
+            </div>
+            <Box
+                sx={{
+                    flexGrow: 1,
+                    overflowY: "auto",
+                    overflowX: "hidden",
+                    maxHeight: "440px",
+                    padding: "1%",
+                    width: "90%",
+                    marginTop: "10px",
+                    "&::-webkit-scrollbar": {
+                        width: "0.3em"
+                    },
+                    "&::-webkit-scrollbar-thumb": {
+                        backgroundColor: "#BEE5B0",
+                        borderRadius: "10px"
+                    }
+                }}
+            >
+                {!user && <CircularProgress className="spinner" />}
+                {listReviews ? (
+                    <div>
+                        {listReviews.length > 0 ? (
+                            <div>
+                                <Grid container spacing={{ xs: 2, md: 3 }}>
+                                    {listReviews.map(review => (
+                                        <Grid item xs={6} key={review}>
+                                            <ReviewPeek reviewData={review} />
+                                        </Grid>
 
-//     // if its the  logged in user, load the myprofile component
-//     const isUser = username === loggedUsername;
-//     if (isUser) {
-//         navigate("/my-profile");
-//     }
-
-//     const userQueryProfile = useQuery("profile", () => getProfile(username));
-//     const { data: userProfile, isLoading } = userQueryProfile;
-
-//     return (
-//         <div className="profile-details">
-//             {isLoading && <CircularProgress className="spinner" />}
-//             {userProfile ? (
-//                 <div>
-//                     <div className="form-control-profile">
-//                         <label>Username </label>
-//                         <input
-//                             type="text"
-//                             name="username"
-//                             id="username"
-//                             value={userProfile.username}
-//                             readOnly="readOnly"
-//                         />
-//                     </div>
-//                     <div className="form-control-profile">
-//                         <label>Email </label>
-//                         <input
-//                             type="text"
-//                             name="email"
-//                             id="email"
-//                             value={userProfile.email}
-//                             readOnly="readOnly"
-//                         />
-//                     </div>
-//                     <div className="form-control-profile-bio">
-//                         <label>Bio </label>
-//                         <textarea
-//                             type="text"
-//                             name="bio"
-//                             id="bio"
-//                             value={userProfile.bio}
-//                             readOnly="readOnly"
-//                         />
-//                     </div>
-//                     <div className="changepw">
-//                         <a href="change-password">Change Password</a>
-//                     </div>
-//                 </div>
-//             ) : (
-//                 <h1>User not found</h1>
-//             )}
-//         </div>
-//     );
-// }
+                                    ))}
+                                </Grid>
+                            </div>
+                        ) : (
+                            // If the info can't be loaded, then display a message
+                            <h2>User has not posted</h2>
+                        )}
+                    </div>
+                ) : (
+                    <h2>Found no reviews</h2>
+                )}
+            </Box>
+        </div>
+    );
+}
 
 function Profile() {
+    //getting visited user
+    const navigate = useNavigate();
+    const { username } = useParams();
+
+    const [user] = useContext(UserContext);
+    const [loggedUsername, setloggedUsername] = useState("");
+    useEffect(() => {
+        setloggedUsername(user?.username);
+        const isUser = username === loggedUsername;
+        if (isUser) {
+            navigate("/my-profile");
+        }
+    }, [user]);
+
+    // if its the  logged in user, load the myprofile component
+    const { data: userProfile, isLoading } = useQuery(
+        "profile",
+        () => getProfile(username),
+        { enabled: !!username }
+    );
+
     return (
         <div className="content-Profile">
-            {/* <span className="bigScreen-Profile">
-                <TopUser />
-                <div className="line5" />
-                <div className="r1">
-                    <Sidebar />
-                    <div className="line6" />
-                    <ProfileDetails />
-                </div>
-            </span>
+            {isLoading && <CircularProgress className="spinner" />}
+            {!isLoading && userProfile ? (
+                <>
+                    <span className="smallScreen-Profile">
+                        <div className="c1">
+                            <h1>{userProfile.username}</h1>
+                            <Avatar
+                                alt="user-profile-image"
+                                // src={user.profileImage}
+                                sx={{ 
+                                    height: 110, 
+                                    width:  110,
+                                    mt: "-40px",
+                                }}
+                            />
+                            {userProfile ? (
+                                <div>
+                                    <div className="form-control-profile">
+                                        <label>Username </label>
+                                        <input
+                                            type="text"
+                                            name="username"
+                                            id="username"
+                                            value={userProfile.username}
+                                            readOnly="readOnly"
+                                        />
+                                    </div>
+                                    <div className="form-control-profile-bio">
+                                        <label>Bio </label>
+                                        <textarea
+                                            type="text"
+                                            name="bio"
+                                            id="bio"
+                                            value={userProfile.bio}
+                                            readOnly="readOnly"
+                                        />
+                                    </div>
+                                    <span className="smallScreen-Profile">
+                                        <div className="viewallreviews">
+                                            <a onClick={() => {
+                                                navigate(`/profile/${userProfile.username}/reviews`);
+                                            }}>
+                                                View their reviews
+                                            </a>
+                                        </div>
+                                    </span>
+
+                                </div>
+                            ) : (
+                                <div>Error</div>
+                            )}
+                        </div>
+                    </span>
+                    <span className="bigScreen-Profile">
+                        <TopUser user={userProfile} />
+                        <div className="line5" />
+                        <div className="r1">
+                            <Sidebar user={userProfile} />
+                            <div className="line6" />
+                            <ReviewsBigScreen user={userProfile} />
+                        </div>
+                    </span>
+                </>
+            ) : (
+                <h1>User not found.</h1>
+            )}
             <div className="footer">
-                <p>copyright © 2022 All-for-one</p>
-            </div> */}
+                <p>Copyright © 2022 All-for-one</p>
+            </div>
         </div>
     );
 }
--- client/src/components/ProfileReviews/index.js
+++ client/src/components/ProfileReviews/index.js
@@ -1,10 +1,282 @@
+import React from "react";
 import "./ProfileReviews.css";
 
-function ProfileReviews() {
-    return (
-        <div className="content-profileReviews">
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "../../actions/UserContext";
+import { useParams, useNavigate } from "react-router-dom";
+import { getOtherReviews } from "../../api";
+import { useQuery } from "react-query";
+import { getProfile } from "../../api";
 
+import "@fontsource/martel-sans";
+
+import ReviewPeek from "../ReviewPeek";
+
+import SearchIcon from "@mui/icons-material/Search";
+import FilterAltIcon from "@mui/icons-material/FilterAlt";
+import PostAddIcon from "@mui/icons-material/PostAdd";
+
+import List from "@mui/material/List";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import CircularProgress from "@mui/material/CircularProgress";
+import Avatar from "@mui/material/Avatar";
+import Grid from "@mui/material/Grid";
+
+function SearchBar() {
+    const data = [
+        { Name: "Calia", Author: "abcd123" },
+        { Name: "David's Hotpot", Author: "xyz789" }
+    ];
+    return (
+        <div className="searchbar">
+            <div className="searchrow">
+                <SearchIcon />
+                <input
+                    type="text"
+                    placeholder="Search"
+                    name="search"
+                    id="search"
+                    // value="{{search}}"
+                    required
+                />
+                <FilterAltIcon />
+            </div>
+            <div className="searchResult"></div>
         </div>
+    );
+}
+
+function Post() {
+    return (
+        <div className="postButton">
+            <span className="smallScreen">
+                <IconButton href="#">
+                    <PostAddIcon
+                        sx={{
+                            bgcolor: "#BEE5B0",
+                            color: "white",
+                            borderRadius: "30px",
+                            fontSize: 50,
+                            position: "fixed",
+                            bottom: "40px",
+                            padding: "2px"
+                        }}
+                    />
+                </IconButton>
+            </span>
+        </div>
+    );
+}
+
+function TopUser(props) {
+    const userProfile = props.user;
+    return (
+        <div className="top-user">
+            <div className="top-user-r1">
+                <Avatar
+                    alt="user-profile-image"
+                    // src={userProfile.profileImage}
+                    sx={{ height: 130, width: 130 }}
+                />
+                <div className="top-user-info">
+                    <h2>{userProfile.username}</h2>
+                    <p>{userProfile.bio}</p>
+                </div>
+            </div>
+            <div className="top-user-rev">
+                <p>
+                    <span className="detail">7</span> reviews
+                </p>
+                <p>
+                    <span className="detail">10k</span> likes
+                </p>
+            </div>
+        </div>
+    );
+}
+
+function Sidebar(props) {
+    const userProfile = props.user;
+    const navigate = useNavigate();
+
+    return (
+        <div className="sidebar-content">
+            <div id="current">
+                <a onClick={() => {
+                    navigate(`/profile/${userProfile.username}/reviews`);
+                    }}
+                >
+                    reviews
+                </a>
+            </div>
+        </div>
+    );
+}
+
+function ReviewsSmallScreen(props) {
+    const user = props.user;
+    const { data: listReviews, isLoading } = useQuery(
+        "reviews", 
+        () =>
+        getOtherReviews(user?._id)
+    );
+
+    return (
+        <div className="reviews">
+            <div className="reviews-content">
+                <List
+                    sx={{
+                        width: "100%",
+                        justifyContent: "center",
+                        ml: "5px",
+                        overflowY: "auto",
+                        overflowX: "hidden",
+                        flexDirection: "column",
+                        "&::-webkit-scrollbar": {
+                            width: "0.3em"
+                        },
+                        "&::-webkit-scrollbar-thumb": {
+                            backgroundColor: "#FFFEEC",
+                            borderRadius: "10px",
+                            maxHeight: "4px"
+                        }
+                    }}
+                >
+                    {isLoading && <CircularProgress className="spinner" />}
+                    {listReviews ? (
+                        <div>
+                            {listReviews.length > 0 ? (
+                                <div>
+                                    {listReviews.map(review => {
+                                        return (
+                                            <ReviewPeek reviewData={review} />
+                                        );
+                                    })}
+                                </div>
+                            ) : (
+                                // If the info can't be loaded, then display a message
+                                <h2>User has not posted</h2>
+                            )}
+                        </div>
+                    ) : (
+                        <h2>Found no reviews</h2>
+                    )}
+                </List>
+            </div>
+        </div>
+    );
+}
+
+function ReviewsBigScreen(props) {
+    const user = props.user;
+    const { data: listReviews, isLoading } = useQuery(
+        "reviews",
+        () => getOtherReviews(user?._id),
+        { enabled: !!user }
+    );
+
+    return (
+        <div className="reviews">
+            <div className="reviews-r1">
+                <h2>reviews</h2>
+                <SearchBar />
+            </div>
+            <Box
+                sx={{
+                    flexGrow: 1,
+                    overflowY: "auto",
+                    overflowX: "hidden",
+                    maxHeight: "440px",
+                    padding: "1%",
+                    width: "90%",
+                    marginTop: "10px",
+                    "&::-webkit-scrollbar": {
+                        width: "0.3em"
+                    },
+                    "&::-webkit-scrollbar-thumb": {
+                        backgroundColor: "#BEE5B0",
+                        borderRadius: "10px"
+                    }
+                }}
+            >
+                {!user && <CircularProgress className="spinner" />}
+                {listReviews ? (
+                    <div>
+                        {listReviews.length > 0 ? (
+                            <div>
+                                <Grid container spacing={{ xs: 2, md: 3 }}>
+                                    {listReviews.map(review => (
+                                        <Grid item xs={6} key={review}>
+                                            <ReviewPeek reviewData={review} />
+                                        </Grid>
+
+                                    ))}
+                                </Grid>
+                            </div>
+                        ) : (
+                            // If the info can't be loaded, then display a message
+                            <h2>User has not posted</h2>
+                        )}
+                    </div>
+                ) : (
+                    <h2>Found no reviews</h2>
+                )}
+            </Box>
+        </div>
+    );
+}
+
+function ProfileReviews() {
+    //getting visited user
+    const navigate = useNavigate();
+    const { username } = useParams();
+
+    const [user] = useContext(UserContext);
+    const [loggedUsername, setloggedUsername] = useState("");
+    useEffect(() => {
+        setloggedUsername(user?.username);
+        const isUser = username === loggedUsername;
+        if (isUser) {
+            navigate("/my-profile");
+        }
+    }, [user]);
+
+    // if its the logged in user, load the myprofile component
+    const { data: userProfile, isLoading } = useQuery(
+        "profile",
+        () => getProfile(username),
+        { enabled: !!username }
+    );
+
+    return (
+        <>
+            {userProfile ? (
+                <div className="content-ProfileReviews">
+                    <span className="smallScreen-ProfileReviews">
+                        <h1> {userProfile.username}'s REVIEWS</h1>
+                        <SearchBar />
+                        <div className="line" />
+                        <ReviewsSmallScreen user={userProfile} />
+                        <Post />
+                    </span>
+                    <span className="bigScreen-ProfileReviews">
+                        <TopUser user={userProfile} />
+                        <div className="line5" />
+                        <div className="r1">
+                            <Sidebar />
+                            <div className="line6" />
+                            <ReviewsBigScreen user={userProfile} />
+                        </div>
+                    </span>
+                    <div className="footer">
+                        <p>Copyright © 2022 All-for-one</p>
+                    </div>
+                </div>
+            ) : (
+                <CircularProgress className="spinner" />
+            )}
+        </>
     );
 }
 
