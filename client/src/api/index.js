b15e36009721dc521bce9f190495ffbc1b374a5a
ccahyadi <ccahyadi@student.unimelb.edu.au>
2022-11-06 15:35:52 +0700

    Revert SERVER_URL to original

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-//const SERVER_URL = "https://foodie-eats.herokuapp.com";
-const SERVER_URL = "http://localhost:5000";
+const SERVER_URL = "https://foodie-eats.herokuapp.com";
 
 axios.interceptors.request.use(
     config => {
